b753cba1852fbb6163cb13365e271ec1ed5088be
KAptNDyaRI <44928763+KAptNDyaRI@users.noreply.github.com>
2019-12-22 23:16:27 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("658330861103742976")
+setInterval(function() {
+channel.send(`kaptnA2 kaptAn kaptn1A kaptnA1 kaptnA1`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
